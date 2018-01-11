94aa187b5ccd20e1e9b61211d413e0313c505f07
Sandhya <sandhyapv20153@gmail.com>
2018-01-10 21:11:36 -0600

    updated

--- src/learningexample/collections/CollectionPrgs.java
+++ src/learningexample/collections/CollectionPrgs.java
@@ -1,6 +1,5 @@
 package learningexample.collections;
 
-import java.util.ArrayList;
 import java.util.*;
 
 public class CollectionPrgs {
@@ -57,6 +56,20 @@ public class CollectionPrgs {
 		
 	}
 	
+	public void ArrayListFunction()
+	{
+		ArrayList<Integer> arrList = new ArrayList<Integer>();
+		arrList.add(10);
+		arrList.add(20);
+		arrList.add(30);
+		Iterator itr = arrList.iterator();
+		System.out.println("************* ArrayList Iteration started *************");
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+	
 	public void QueueUsingLinkedList()
 	{
 		Queue<String> q = new LinkedList<String>();
@@ -90,8 +103,28 @@ public class CollectionPrgs {
 		
 	}
 	
-	public void MapUsingHashMap()
+	public void HashMapFunction()//HashMap is not threadsafe but it allows null value
 	{
+		HashMap<Integer,String> map = new HashMap<Integer,String>();
+		map.put(01, "One");
+		map.put(02, "Two");
+		System.out.println("the hashmap..."+map.get(02));
+		System.out.println("************* HasMap Iteration started *************");
+		for(Map.Entry<Integer,String> entry:map.entrySet())
+		{
+			int key = entry.getKey();
+			String value = entry.getValue();
+			System.out.println(key);
+		}
+		
+	}
+	
+	public void HashTableFunction()//Hashtable is threadsafe and it does not allow null value
+	{
+		Hashtable<Integer,String> table = new Hashtable<Integer,String>();
+		table.put(01, "One");
+		table.put(02, "Two");
+		System.out.println("the hastable.."+table.get(01));
 		
 	}
 	
@@ -102,6 +135,9 @@ public class CollectionPrgs {
 		obj.ListUsingArrayListFunction();
 		obj.ListUsingLinkedListFunction();
 		obj.QueueUsingLinkedList();
+		obj.HashMapFunction();
+		obj.HashTableFunction();
+		obj.ArrayListFunction();
 
 
 	}
