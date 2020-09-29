d7809415c97a9df76c724bf7af629749bfb43a78
Sandhya <sandhyapv20153@gmail.com>
2020-09-29 13:30:30 -0500

    added uuid

---
+++ src/learningexample/uuidgenerator/GenerateUUID.java
@@ -0,0 +1,15 @@
+package learningexample.uuidgenerator;
+
+import java.util.*;
+
+public class GenerateUUID {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 UUID uid = UUID.fromString("11200000-8cf0-12bd-b23e-10b96e4ef00d");     
+
+	     System.out.println("Random UUID value: "+uid.randomUUID()); 
+
+	}
+
+}
