e7e24e0469601264f816fc2ff456c9815b56d9f3
Sandhya <sandhyapv20153@gmail.com>
2020-09-30 15:00:29 -0500

    modified hashmap method

--- src/learningexample/collections/CollectionPrgs.java
+++ src/learningexample/collections/CollectionPrgs.java
@@ -18,15 +18,15 @@ public class CollectionPrgs {
 		array.remove(2);
 		Iterator itr = array.iterator();
 		String str;
-		System.out.println("******* ArrayList Iteration started *******");
+		System.out.println("******* ArrayList started *******");
 		while(itr.hasNext())
 		{
 			str = itr.next().toString();
 			System.out.println(str);
 		}
-		System.out.println("******* ArrayList Iteration Ended *******");
 		System.out.println("The third element of the arraylist is :"+array.get(2));
 		System.out.println("The size of the arraylist is :"+array.size());
+		System.out.println("******* ArrayList Ended *******\n");
 		
 		
 	}
@@ -44,15 +44,15 @@ public class CollectionPrgs {
 		list.remove(3);
 		Iterator itr = list.iterator();
 		String str;
-		System.out.println("******* LinkedList Iteration started *******");
+		System.out.println("******* LinkedList started *******");
 		while(itr.hasNext())
 		{
 			str = itr.next().toString();
 			System.out.println(str);
 		}
-		System.out.println("******* LinkedList Iteration Ended *******");
 		System.out.println("The third element of the linkedlist is :"+list.get(2));
 		System.out.println("The size of the linkedlist is :"+list.size());
+		System.out.println("******* LinkedList Ended *******\n");
 		
 	}
 	
@@ -63,11 +63,12 @@ public class CollectionPrgs {
 		arrList.add(20);
 		arrList.add(30);
 		Iterator itr = arrList.iterator();
-		System.out.println("************* ArrayList Iteration started *************");
+		System.out.println("************* ArrayList started *************");
 		while(itr.hasNext())
 		{
 			System.out.println(itr.next());
 		}
+		System.out.println("************* ArrayList ended *************\n");
 	}
 	
 	public void QueueUsingLinkedList()
@@ -77,29 +78,30 @@ public class CollectionPrgs {
 		q.add("Two");
 		q.add("Three");
 		q.add("Four");
+		System.out.println("************* queue using LinkedList started *************");
 		System.out.println("The size of the queue is :"+q.size());
 		Iterator itr= q.iterator();
 		String str;
-		System.out.println("******* queue using LinkedList Iteration started *******");
+		System.out.println("****queue using LinkedList Iteration started ****");
 		while(itr.hasNext())
 		{
 			str = itr.next().toString();
 			System.out.println(str);
 		}		
-		System.out.println("******* queue using LinkedList Iteration ended *******");
+		System.out.println("****queue using LinkedList Iteration ended ****");
 		q.remove(3);//this does not work
 		q.remove("Three");
 		itr=q.iterator();
-		System.out.println("******* queue using LinkedList Iteration after remove *******");
+		System.out.println("***queue using LinkedList Iteration after remove***");
 		while(itr.hasNext())
 		{
 			str = itr.next().toString();
 			System.out.println(str);
 		}		
-		System.out.println("******* queue using LinkedList Iteration ended *******");
 		System.out.println("The head of the queue before poll is :"+q.poll());
 		System.out.println("The head of the queue after poll is :"+q.peek());
 		System.out.println("The size of the queue is :"+q.size());
+		System.out.println("******* queue using LinkedList Iteration ended *******\n");
 		
 	}
 	
@@ -109,13 +111,12 @@ public class CollectionPrgs {
 		map.put(01, "One");
 		map.put(02, "Two");
 		System.out.println("the hashmap..."+map.get(02));
-		System.out.println("************* HasMap Iteration started *************");
-		for(Map.Entry<Integer,String> entry:map.entrySet())
-		{
-			int key = entry.getKey();
-			String value = entry.getValue();
-			System.out.println(key);
+		System.out.println("************* HasMap started *************");
+		Set<Integer> keys = map.keySet();
+		for(Integer key: keys) {
+			System.out.println("Key:"+key+"Values:"+map.get(key));
 		}
+		System.out.println("************* HasMap ended *************\n");
 		
 	}
 	
@@ -126,11 +127,13 @@ public class CollectionPrgs {
 		table.put(02, "Two");
 		table.put(03, "Three");
 		table.put(04, "Four");
+		System.out.println("************* Hashtable started *************");
 		System.out.println("the hastable.."+table.get(01));
 		Set<Integer> keys = table.keySet();
 		for(Integer key:keys) {
 			System.out.println("The Key:"+key+"\tValue:"+table.get(key));
 		}
+		System.out.println("************* Hashtable ended *************");
 		
 
 		
