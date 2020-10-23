f22e4a9336018ed0212cd3e93af6bf8d5dd872fd
sveettil <sandhya.veettil@ticketmaster.com>
2020-10-23 16:29:32 -0500

    updated json java file

--- src/learningexample/jsonobjects/JSONObjectManipulation.java
+++ src/learningexample/jsonobjects/JSONObjectManipulation.java
@@ -6,24 +6,8 @@ import java.util.*;
 
 public class JSONObjectManipulation {
 	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		JSONObject jObj = new JSONObject();
-		jObj.put("name","Mary");
-		jObj.put("age",30);
-		
-		JSONObject address = new JSONObject();
-		address.put("StreetAddress","Test Street 1");
-		address.put("City","Test City");
-		address.put("ZipCode","123456");
-		jObj.put("address",address);
-		
-		System.out.println("Json Object :"+jObj+"\n");
-		
-	
+	public void jsonObjectIterator(JSONObject jObj )
+	{
 		//Parsing json
 		Iterator<String> keys= jObj.keys();
 		String keyValue;
@@ -49,7 +33,62 @@ public class JSONObjectManipulation {
 				System.out.println(keyValue +"\t: "+jObj.get(keyValue));
 			}
 		}
+
+	}
+	
+	
+	public void jsonArrayIterator(JSONObject personObj)
+	{
+		Iterator<String> keys = personObj.keys();
+		String keyValue;
+		while(keys.hasNext())
+		{
+			keyValue = keys.next();
+			System.out.println(keyValue + "\t:"+personObj.get(keyValue));
+		}
 		
 	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JSONObject jObj = new JSONObject();
+		jObj.put("name","Mary");
+		jObj.put("age",30);
+		
+		JSONObject address = new JSONObject();
+		address.put("StreetAddress","Test Street 1");
+		address.put("City","Test City");
+		address.put("ZipCode","123456");
+		jObj.put("address",address);
+		
+		System.out.println("Json Object :"+jObj+"\n");
+		
+		JSONObjectManipulation jManipulateObj = new JSONObjectManipulation();
+		jManipulateObj.jsonObjectIterator(jObj);
+		
+		
+		JSONObject personObj = new JSONObject();
+		personObj.put("name","John");
+		personObj.put("age", 40);
+		JSONArray jArray = new JSONArray();// used to store multiple values
+		jArray.put("Fiesta");
+		jArray.put("Focus");
+		jArray.put("Mustang");
+		personObj.put("cars", jArray);
+		
+		System.out.println("\nJson "+personObj);
+		jManipulateObj.jsonArrayIterator(personObj);
+		
+		
+		
+		
+		
+		
+ 
+	}
+
 
 }
